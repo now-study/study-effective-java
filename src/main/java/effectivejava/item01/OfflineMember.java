4141aacd9f231b9050cb705e5cb6f255bfaf1950
Daeho Ro <lamanus@outlook.kr>
2022-09-26 13:32:21 +0900

    delete init sources

--- src/main/java/effectivejava/item01/OfflineMember.java
+++
@@ -1,9 +0,0 @@
-package main.java.effectivejava.item01;
-
-public class OfflineMember extends Member {
-
-	public OfflineMember(String username, String email) {
-		super(username, email, OFFLINE_TYPE);
-	}
-
-}
--- src/main/java/effectivejava/item01/OnlineMember.java
+++
@@ -1,9 +0,0 @@
-package main.java.effectivejava.item01;
-
-public class OnlineMember extends Member {
-
-	public OnlineMember(String username, String email) {
-		super(username, email, ONLINE_TYPE);
-	}
-
-}
